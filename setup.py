2eebd23309a89d682787707d002cdb4751ae2e9c
Jason <jason@bydesignpublishing.com>
2021-09-16 12:55:49 -0700

    remove setup

--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name='split',
-    version='.1',
-    install_requires=open('requirements.txt').read().splitlines(),
-    packages=setuptools.find_packages()
-)
