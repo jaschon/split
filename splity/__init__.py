354215e9753e5bebe853f41f5b0037e537e209e0
Jason <jason.rebuck@gmail.com>
2022-05-12 13:20:04 -0700

    fix dup folders and files problem. put all items in a unique folder. split multiple files when in split mode.

--- splity/__init__.py
+++ splity/__init__.py
@@ -5,10 +5,18 @@ Tools to split/join pdf files
 
 __author__ = "Jason Rebuck"
 __copyright__ = "2011-2022"
-__version__ = "0.30"
+__version__ = "0.35"
 
 import os
 import PyPDF2
+import uuid
+
+def make_dir(path, extra=""):
+    root, ext =  os.path.splitext(os.path.basename(path).replace(" ", "_").replace("/", "-"))                    
+    folder = os.path.join(os.path.dirname(path), extra, root)
+    folder = f'{folder}_{uuid.uuid4()}' if os.path.isdir(folder) else folder
+    os.makedirs(folder)
+    return folder
 
 def split_pdf(paths):
     """Splits pdf files into separate files."""
@@ -16,14 +24,13 @@ def split_pdf(paths):
         if path.endswith(".pdf"):
             try:
                 with open(path, 'rb') as file_p:
-                    path_root, path_ext = os.path.splitext(os.path.basename(path).replace(" ", "_"))                    
-                    new_dir_name = os.path.dirname(path)
+                    folder = make_dir(path, "split")
                     pdf_read = PyPDF2.PdfFileReader(file_p, False)
                     for page in range(pdf_read.getNumPages()):                        
-                        new_path_name = os.path.join(new_dir_name, f"{page+1}{path_ext}")
+                        file_name = os.path.join(folder, f"{page+1:3}.pdf")
                         pdf_write = PyPDF2.PdfFileWriter()
                         pdf_write.addPage(pdf_read.getPage(page))
-                        with open(new_path_name, 'wb') as file_write:
+                        with open(file_name, 'wb') as file_write:
                             pdf_write.write(file_write)
             except (OSError, PyPDF2.utils.PdfReadError) as e:
                 print(f"• {e}")
@@ -32,12 +39,14 @@ def join_pdf(paths):
     """Joins pdf file list to a single file."""
     pdf_merger = PyPDF2.PdfFileMerger(False)
     try:
-        new_file_name = os.path.join(os.path.dirname(paths[0]), "combo.pdf")
+        file_name = os.path.join(make_dir(paths[0], "join"), f"joined[{len(paths)}].pdf")
         for path in paths:
             path = os.path.abspath(path)
-            if path.endswith(".pdf") and path != new_file_name:
+            try:
                 pdf_merger.append(path)
-        pdf_merger.write(new_file_name)
+            except (OSError, PyPDF2.utils.PdfReadError) as e:
+                print(f"• {e}")
+        pdf_merger.write(file_name)
     except (OSError, PyPDF2.utils.PdfReadError) as e:
         print(f"• {e}")
     finally:
