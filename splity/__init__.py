e45c3f3c4a828f3aaf55ee77593c8e10662c37bd
Jason <jason.rebuck@gmail.com>
2022-09-21 09:37:41 -0700

    add zero fill flag. better path cleaning

--- splity/__init__.py
+++ splity/__init__.py
@@ -5,20 +5,21 @@ Tools to split/join pdf files
 
 __author__ = "Jason Rebuck"
 __copyright__ = "2011-2022"
-__version__ = "0.355"
+__version__ = "0.37"
 
 import os
 import PyPDF2
 import uuid
+import re
 
 def make_dir(path, extra=""):
-    root, ext =  os.path.splitext(os.path.basename(path).replace(" ", "_").replace("/", "-"))                    
+    root, ext =  os.path.splitext(re.sub('[^0-9a-zA-Z\.]+', '_', os.path.basename(path)))                    
     folder = os.path.join(os.path.dirname(path), extra, root)
     folder = f'{folder}_{uuid.uuid4()}' if os.path.isdir(folder) else folder
     os.makedirs(folder)
     return folder
 
-def split_pdf(paths):
+def split_pdf(paths, fill=3):
     """Splits pdf files into separate files."""
     for path in paths:
         if path.endswith(".pdf"):
@@ -27,7 +28,7 @@ def split_pdf(paths):
                     folder = make_dir(path, "split")
                     pdf_read = PyPDF2.PdfFileReader(file_p, False)
                     for page in range(pdf_read.getNumPages()):                        
-                        file_name = os.path.join(folder, f"{page+1:03}.pdf")
+                        file_name = os.path.join(folder, f"{page+1:0{fill}}.pdf")
                         pdf_write = PyPDF2.PdfFileWriter()
                         pdf_write.addPage(pdf_read.getPage(page))
                         with open(file_name, 'wb') as file_write:
